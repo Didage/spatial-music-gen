244728787e9636103639f62d9cedbe0938938584
Alejandro Tovar <da.tovar10@uniandes.edu.co>
2023-10-17 14:29:56 -0500

    Audio player component improved

--- front/src/components/AudioPlayer.js
+++ front/src/components/AudioPlayer.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import ReactAudioPlayer from 'react-audio-player';
+import React, { useState, useEffect } from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 function AudioPlayer() {
   const [selectedAudio, setSelectedAudio] = useState(null);
   const [audioFiles, setAudioFiles] = useState([]);
-  const [selectedFile, setSelectedFile] = useState('');
+  const [selectedFile, setSelectedFile] = useState("");
 
   // Fetch the list of audio files from the server
   useEffect(() => {
-    fetch('localhost:8000/generations') // Replace with your server's endpoint
+    fetch("http://localhost:8000/generations") // Replace with your server's endpoint
       .then((response) => response.json())
       .then((data) => setAudioFiles(data));
   }, []);
 
   const handleFileSelection = (event) => {
-    const fileURL = event.target.value;
+    const fileURL = ("./" + event.target.value).replace("musicge", "musicgen");
     setSelectedAudio(fileURL);
   };
 
@@ -24,24 +24,23 @@ function AudioPlayer() {
       <select onChange={handleFileSelection}>
         <option value="">Select an audio file</option>
         {audioFiles.map((file, index) => (
-          <option key={index} value={file.audio1URL}>
-            {file.name}
-          </option>
-        ))}
-        {audioFiles.map((file, index) => (
-          <option key={index} value={file.audio2URL}>
-            {file.name}
-          </option>
-        ))}
-        {audioFiles.map((file, index) => (
-          <option key={index} value={file.audio3URL}>
-            {file.name}
-          </option>
+          <>
+            <option key={index} value={file.audio1URL}>
+              {file.audio1URL}
+            </option>
+            <option key={index} value={file.audio2URL}>
+              {file.audio2URL}
+            </option>
+            <option key={index} value={file.audio3URL}>
+              {file.audio3URL}
+            </option>
+          </>
         ))}
       </select>
       {selectedAudio && (
         <div>
-          <ReactAudioPlayer src={selectedAudio} controls autoPlay />
+            {selectedAudio}
+          <ReactAudioPlayer src="./audios/Gen0-musicgen-outA.wav" controls autoPlay />
         </div>
       )}
     </div>
